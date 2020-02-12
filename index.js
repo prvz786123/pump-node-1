5d5429b3792b9a9cc4cce0201c925f39cba746a7
PARVEZ <parvez.s@canishub.com>
2020-02-12 16:51:45 +0530

    test commit 1

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.get('/',(req,res)=>{
     res.send({success:true,message:"Hello This is working fine"})
 });
 
-app.listen(process.env.PORT);
+app.get("/test",(req,res)=>{
+    res.send({success:true,message:"This is test route"})
+})
+
+app.listen(port);
